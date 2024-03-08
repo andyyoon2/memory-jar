0db7cd01d41def13a85498f0cf4167cc252a889b
Andy Yoon <insoo.yoon@proton.me>
2024-03-08 15:03:15 -0800

    Add data types and more API operations

--- web/app/api/users/route.ts
+++ web/app/api/users/route.ts
@@ -1,5 +1,7 @@
 import * as uuid from 'uuid';
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, ScanCommand, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
+import { AttributeValue as attr, updateExpr } from 'dynamodb-data-types';
+import { USER_PREFIX } from '../constants';
 
 const client = new DynamoDBClient({});
 
@@ -8,22 +10,44 @@ export async function POST(request: Request) {
   if (!body.name) {
     return Response.json({ error: "Name is required" }, { status: 400 });
   }
-  const userId = `u#${uuid.v4()}`;
-  console.log(userId);
 
-  const response = await client.send(
-    new PutItemCommand({
+  const userId = `${USER_PREFIX}${uuid.v4()}`;
+  const Key = attr.wrap({
+    PK: userId,
+    SK: userId,
+  });
+  const update = updateExpr()
+    .set({ GSI1_PK: userId })
+    .set({ GSI1_SK: userId })
+    .set({ uName: body.name })
+    .expr();
+
+  const { Attributes } = await client.send(
+    new UpdateItemCommand({
       TableName: process.env.TABLE_NAME,
-      Item: {
-        PK: { S: userId },
-        SK: { S: userId },
-        uData: { M: { name: { S: body.name } } },
-        "GSI1-PK": { S: userId },
-        "GSI1-SK": { S: userId },
-      }
+      Key,
+      ...update,
+      ReturnValues: "ALL_NEW",
     })
   );
   // TODO: Error handling
 
-  return Response.json(response);
+  const newUserRecord = attr.unwrap(Attributes ?? {});
+
+  return Response.json({ id: newUserRecord.PK.substring(USER_PREFIX.length) });
 }
+
+// SCAN - TODO: only for debugging purposes
+export async function GET() {
+  const { Items } = await client.send(
+    new ScanCommand({
+      TableName: process.env.TABLE_NAME,
+      FilterExpression: "contains(PK, :prefix)",
+      ExpressionAttributeValues: { ":prefix": { S: "u#" } },
+    })
+  );
+
+  const users = Items?.map(attr.unwrap);
+  return Response.json(users);
+}
+
