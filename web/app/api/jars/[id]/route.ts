7476916992589d1fc26850976c7d4fd631173363
Andy Yoon <insoo.yoon@proton.me>
2024-03-17 11:10:43 -0700

    Get jar info and users by jar

--- web/app/api/jars/[id]/route.ts
+++ web/app/api/jars/[id]/route.ts
@@ -1,8 +1,65 @@
-import { DynamoDBClient, BatchWriteItemCommand, UpdateItemCommand, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, BatchWriteItemCommand, UpdateItemCommand, QueryCommand, GetItemCommand } from '@aws-sdk/client-dynamodb';
 import { AttributeValue as attr, updateExpr } from 'dynamodb-data-types';
 import { JAR_PREFIX, USER_PREFIX } from '../../utils';
 
 const client = new DynamoDBClient({});
+
+/** Get jar info and all users in a jar */
+export async function GET(_request: Request, { params }: { params: { id: string } }) {
+  const TABLE_NAME = process.env.TABLE_NAME;
+  if (!TABLE_NAME) {
+    return Response.json({ error: "Error reading table name" }, { status: 500 });
+  }
+
+  const jarId = `${JAR_PREFIX}${params.id}`;
+  const Key = attr.wrap({ PK: jarId, SK: jarId });
+  const getJarCommand = new GetItemCommand({
+    TableName: TABLE_NAME,
+    Key,
+    ProjectionExpression: "PK, mjName, mjColor, mjIcon"
+  });
+
+  // Retrieve all users in this jar. TODO: Handle pagination if necessary (BatchGetItems can get 100 max at once)
+  const ExpressionAttributeValues = attr.wrap({
+    ":jarId": jarId,
+    ":userPrefix": USER_PREFIX,
+  });
+  const queryUsersCommand = new QueryCommand({
+    TableName: TABLE_NAME,
+    KeyConditionExpression: "PK = :jarId and begins_with(SK, :userPrefix)",
+    ExpressionAttributeValues,
+    ProjectionExpression: "SK, uName, uImage",
+    ScanIndexForward: false,
+  });
+
+  // Make the request
+  const [{ Item: jarRecord }, { Items: userRecords }] = await Promise.all([
+    client.send(getJarCommand),
+    client.send(queryUsersCommand),
+  ]);
+
+  // Build the API response
+  const jar = attr.unwrap(jarRecord);
+  const result = {
+    jar: {
+      id: jar.PK.substring(JAR_PREFIX.length),
+      name: jar.mjName,
+      color: jar.mjColor,
+      icon: jar.mjIcon,
+    },
+    users: [] as any[], // TODO: Type definitions for all API results etc.
+  };
+  (userRecords ?? []).forEach(userRecord => {
+    const user = attr.unwrap(userRecord);
+    result.users.push({
+      id: user.SK.substring(USER_PREFIX.length),
+      name: user.uName,
+      image: user.uImage,
+    });
+  });
+
+  return Response.json(result);
+}
 
 /** Add a user to a jar */
 export async function PUT(request: Request, { params }: { params: { id: string } }) {
@@ -11,9 +68,20 @@ export async function PUT(request: Request, { params }: { params: { id: string }
     return Response.json({ error: "User ID is required" }, { status: 400 });
   }
 
-  // Build UpdateCommand for new jar->user record
   const jarId = `${JAR_PREFIX}${params.id}`;
   const userId = `${USER_PREFIX}${body.userId}`;
+
+  // Get user data
+  const { Item: userRecord } = await client.send(
+    new GetItemCommand({
+      TableName: process.env.TABLE_NAME,
+      Key: attr.wrap({ PK: userId, SK: userId })
+    })
+  );
+  const user = attr.unwrap(userRecord);
+  console.log('got user', JSON.stringify(user));
+
+  // Build UpdateCommand for new jar->user record
   const Key = attr.wrap({
     PK: jarId,
     SK: userId,
@@ -22,7 +90,10 @@ export async function PUT(request: Request, { params }: { params: { id: string }
     .set({ GSI1_PK: userId })
     .set({ GSI1_SK: jarId })
     .set({ mjName: body.name })
+    .set({ uName: user?.uName })
+    .set({ uImage: user?.uImage })
     .expr();
+  console.log(jarUpdateExpr);
 
   const { Attributes } = await client.send(
     new UpdateItemCommand({
@@ -61,6 +132,7 @@ export async function DELETE(_request: Request, { params }: { params: { id: stri
       KeyConditionExpression: "PK = :jarId and begins_with(SK, :userPrefix)",
       ExpressionAttributeValues,
       ProjectionExpression: "SK",
+      ScanIndexForward: false,
     })
   );
   console.log('got items', Items);
